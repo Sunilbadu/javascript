b2bf2497defae459b01a18a2b43755ec88a0405e
Sunilbadu <sunylbadu7@gmail.com>
2026-05-08 12:46:12 +0545

    day 1 completed

---
+++ day1.js
@@ -0,0 +1,22 @@
+// pracice set 1
+
+var a = "sunil"
+var a = 5
+console.log(a)
+console.log(typeof a)
+let b = 5
+b= "badu"
+
+const kasika = 8
+console.log(kasika)
+{
+    // kasika= 7
+    console.log(`my name is ${kasika}`)
+}
+
+const person ={
+    name: "sunil"
+}
+person.name = 5
+console.log(typeof person.name)
+
