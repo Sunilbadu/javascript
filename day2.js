20ae8c4b4a5c61530a87439716af7b893ccb605d
Sunilbadu <sunylbadu7@gmail.com>
2026-05-08 12:47:16 +0545

    day 2(I)

---
+++ day2.js
@@ -0,0 +1,52 @@
+// let a = prompt("enetr the age of the person");
+// if (a>10 && a<20)
+// {
+//     console.log("yes the person lies in that particular age limit");
+// }
+// else{
+//     console.log("no the person doesnt lies in that range")
+// }
+
+// qno 2
+//  let b = Number(prompt("enter the age of the person"))
+// switch (true){
+
+//     case(b<10):
+//     console.log("you are an infant")
+//     break
+
+
+//     case(b>10 && b<20):
+//     console.log("you are teen")
+//     break
+
+//     default:
+//         console.log("you are na adult");
+//         break;
+// }
+
+//  qno 3
+
+// let n =prompt("enter a number")
+// if (n%2 === 0 && n%3===0){
+//     console.log("yes the given number is valid")
+// }
+// else{
+//     console.log("invalid");
+// }
+
+//qno 4
+
+// let n =prompt("enter a number")
+// if (n%2 === 0 || n%3===0){
+//     console.log("yes the given number is valid")
+// }
+// else{
+//     console.log("invalid");
+// }
+
+//qno 6
+
+
+let Age = Number(prompt("enter the age of the person"))
+let x=( Age > 18)?console.log("you can drive "):console.log("you cant drive")
