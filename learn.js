2360297d3bb4c25da5733de43ce296901ba83f9a
Sunilbadu <sunylbadu7@gmail.com>
2026-05-08 15:20:46 +0545

    function and loops

---
+++ learn.js
@@ -0,0 +1,32 @@
+let marks={
+    harry : 10,
+    ram: 20,
+    kapil : 22
+}
+// for (let i = 0 ; i  < Object.keys(marks).length ; i++ ){
+//     console.log(marks[Object.keys(marks)[i]]);
+
+// }
+
+//qno 2 
+// for (let b in marks){
+//     console.log("the marks of "+b+" is "+marks[b]);
+// }
+
+// let a=50
+// let c
+// while( a != c){
+//      c=Number(prompt("enter a random guessing number"))
+// }
+//     console.log("the correct guess is"+c)
+
+let a=10;
+let b= 10
+let c= 10
+let d=10
+let e = 10
+let average = (q,w,e,r,t) =>{
+    let avg = (q+w+e+r+t)/5
+    console.log("the average of the given number is="+ avg)
+}
+average(a,b,c,d,e)
