c465509c25c076752f2ee2e161ff160cf851d202
Sunilbadu <sunylbadu7@gmail.com>
2026-05-09 21:18:20 +0545

    p 6

---
+++ another.js
@@ -0,0 +1,10 @@
+let arr =[]
+// for(let i = 0 ; i <5 ; i++){
+//     arr[i]=prompt("enter the element one by one")
+// }
+let i=0
+console.log(arr)
+while(arr[i+1] != 0){
+    arr[i]=prompt("enter the element one by one")
+    i++
+}
