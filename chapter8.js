8b2519ca9b8ab23508dda48e866c2b42a0fa60ac
Sunilbadu <sunylbadu7@gmail.com>
2026-05-13 17:23:18 +0545

    query selection in js

---
+++ chapter8.js
@@ -0,0 +1,8 @@
+// document.getElementById("youtube").addEventListener("click", function() {
+//     let des = "https://amazon.com"
+//     window.open(des)
+// });
+
+setInterval(async function () {
+    document.querySelector(".bulb").classList.toggle("bulb-container")
+},300)
