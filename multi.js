923081e67c81e24ba9350462fa0658c28b722815
Sunilbadu <sunylbadu7@gmail.com>
2026-05-23 19:36:59 +0545

    promise API

--- multi.js
+++ multi.js
@@ -4,12 +4,14 @@ let p1 = new Promise((resolve , reject)=>{
         resolve(1)
     }, 2000);
 })
+
 p1.then(() =>{
     setTimeout(() => {
         
         console.log("hurrah......!")
     }, 2000);
 })
+
 p1.then((value)=>{
     setTimeout(() => {
         console.log(value)
---
+++ promiseapi.js
@@ -0,0 +1,42 @@
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        reject(new Error("value1"))
+    }, 1000);
+})
+
+let p2 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve("value2")
+    }, 2000);
+})
+
+// let p3 = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("value")
+//     }, 3000);
+// })
+let p3 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        reject(new Error ("value"))    //if reject then .all chaldena
+    }, 3000);
+})
+
+
+// let x = Promise.all([p1,  p2, p3])
+// x.then((value)=>{
+//     console.log(value) //we get the value of all promises in the form of array and it executes in the max
+// })
+
+// let x = Promise.allSettled([p1,p2,p3])
+// x.then((value)=>{
+//     console.log(value)
+// })
+
+// let x = Promise.race([p1,p2,p3])
+// x.then((value)=>{
+//     console.log(value)
+// })
+let x = Promise.any([p1,p2,p3])
+x.then((value)=>{
+    console.log(value)
+})
