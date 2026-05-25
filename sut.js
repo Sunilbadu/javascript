32f367294934de40f24245b4369bed5a7f80c447
Sunilbadu <sunylbadu7@gmail.com>
2026-05-25 23:04:08 +0545

    practice 2

--- sut.js
+++ sut.js
@@ -2,3 +2,26 @@ let user = document.getElementById("un").textContent
 let pass = document.getElementById("passw").value
 let x =  document.getElementsByTagName("button")[0]
 x.
+// Set a cookie
+document.cookie = "username=Sunil; expires=Fri, 31 Dec 2026 23:59:59 GMT; path=/";
+
+// Read cookies
+console.log(document.cookie);
+
+// Update a cookie
+document.cookie = "username=SunilBadu; expires=Fri, 31 Dec 2026 23:59:59 GMT; path=/";
+
+// Delete a cookie (set expiry in the past)
+document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+
+// Set a cookie
+document.cookie = "username=Sunil; expires=Fri, 31 Dec 2026 23:59:59 GMT; path=/";
+
+// Read cookies
+console.log(document.cookie);
+
+// Update a cookie
+document.cookie = "username=SunilBadu; expires=Fri, 31 Dec 2026 23:59:59 GMT; path=/";
+
+// Delete a cookie (set expiry in the past)
+document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
