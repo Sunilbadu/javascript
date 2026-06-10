608ae3a1d70587aff04dc79d5850389e1503bc92
Sunilbadu <sunylbadu7@gmail.com>
2026-06-10 21:47:58 +0545

    session and local storage

--- local.js
+++ local.js
@@ -13,3 +13,19 @@ localStorage.removeItem("username");
 
 // Clear all localStorage data
 localStorage.clear();
+
+// Save data to sessionStorage
+sessionStorage.setItem("username", "Sunil");
+
+// Retrieve data from sessionStorage
+let user = sessionStorage.getItem("username");
+console.log(user); // Output: Sunil
+
+// Update data in sessionStorage
+sessionStorage.setItem("username", "SunilBadu");
+
+// Remove a specific item
+sessionStorage.removeItem("username");
+
+// Clear all sessionStorage data
+sessionStorage.clear();
